bfb24dcdc46099a8395b104ef55e1bbc065274cd
Brian <bdinh98@gmail.com>
2018-11-06 09:30:14 -0500

    Created SeaFoodImage and ConfidneceRatingDisplay classes to help with GalleryView class construction

---
+++ app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/ConfidenceRatingDisplay.java
@@ -0,0 +1,16 @@
+package ceg4110.fa2018.group21.snapasnack;
+
+import android.media.Image;
+
+public class ConfidenceRatingDisplay
+{
+    private Image ratingGauge;
+    private int scale;
+
+    public ConfidenceRatingDisplay(int scale)
+    {
+        this.scale = scale;
+    }
+
+
+}
--- app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/GalleryView.java
+++ app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/GalleryView.java
@@ -5,30 +5,45 @@ import android.content.Context;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 // GalleryView should be able to communicate with the API
 // GalleryView should only be a view??
 // Initialize this class and pop up this view when "Browse Community Uploads" is clicked in MainMenuView
-public class GalleryView extends View {
-
+// account for empty case
+public class GalleryView extends View
+{
     private Button mainMenuBtn;
-    // private conf_rating List <ConfidenceRatingDisplay>
+    private ArrayList<SeaFoodImage> images;
 
 
-
-    public GalleryView(Context context) {
+    public GalleryView(Context context)
+    {
         super(context);
-        System.out.println("HELP");
+        InitalizeImages();
+        RetrieveImages(images);
     }
 
-    private void RetrieveImages()
+    public void InitalizeImages()
     {
-        // use this method to populate images into gallery
+        // communicate with swagger UI to create SeaFoodImage objects.
+        //SeaFoodImage completedImage = new SeaFoodImage(SwaggerUI.getImage, SwaggerUI.getConfidenceRating);
+
+    }
+
+    public void RetrieveImages(ArrayList<SeaFoodImage> completedImages)
+    {
+
+        for (int x = 0; x < completedImages.size(); x++)
+        {
+            // populate based on index
+        }
     }
 
     // we need a method for the to handle events for the main menu. this will take the user back to the main menu screen
 
     // also need a method to handle when the user clicks on the image in the gallery. images should be able to be
-    // clicked on and handled accordingly
+    // clicked on and handled accordingly, should be able to add comments as well
 
 
     // maybe we can import libraries instead of implementing from scratch?
