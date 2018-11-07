b2cdc8538cae66a47b2853adfe33f16b716e620a
Matt Barrett <barrett.77@wright.edu>
2018-11-06 23:14:07 -0500

    POST now works for images. Cannot retrieve response at this point

--- app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/ImageSubmissionHelper.java
+++ app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/ImageSubmissionHelper.java
@@ -2,20 +2,27 @@ package ceg4110.fa2018.group21.snapasnack;
 
 import android.content.Context;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ImageSubmissionHelper {
 
     private static RequestQueue _requestQueue;
     private static StringRequest _stringRequest;
+    public static Retrofit _retrofit;
+    private static String _remoteAddr = "http://ec2-13-58-18-68.us-east-2.compute.amazonaws.com";
     private static ImageSubmissionHelper _instance;
-    private static String remoteAddr = "http://ec2-13-58-18-68.us-east-2.compute.amazonaws.com/images/";
-    private static Context _context;
 
     private ImageSubmissionHelper() {
         _requestQueue = null;
@@ -34,23 +41,39 @@ public class ImageSubmissionHelper {
     }
 
     // Returns reponse as a String
-    public static String submitStringRequest(String string, final Context context) {
-        String retVal = "";
-        _requestQueue = Volley.newRequestQueue(context);
-        _stringRequest = new StringRequest(Request.Method.GET, remoteAddr,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        String responseee = response;
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
+    public static String uploadImageFromPath(String filePath, final Context context) {
+        final String[] retVal = {""};
 
-                    }
-                });
-        _requestQueue.add(_stringRequest);
-        return retVal;
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .build();
+        _retrofit = new Retrofit.Builder()
+                .baseUrl(_remoteAddr)
+                .client(okHttpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        UploadApis uploadAPIs = _retrofit.create(UploadApis.class);
+        File file = new File(filePath);
+        RequestBody fileRequestBody = RequestBody.create(MediaType.parse("image/*"), file);
+        MultipartBody.Part part = MultipartBody.Part.createFormData("images", file.getName(), fileRequestBody);
+        RequestBody description = RequestBody.create(MediaType.parse("text/plain"), "image-type");
+        Call call = uploadAPIs.uploadImage(part, description);
+        // NOTE: THIS REQUIRES INTERNET AND EXTERNAL READ/WRITE STORAGE PERMISSIONS
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, retrofit2.Response response) {
+                // TODO: Do something with the response
+                retVal[0] = response.toString();
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                // TODO: Do something if we cannot submit the image
+            }
+        });
+
+        // TODO: The return statement here is called before the response gets back to us...fix?
+        return retVal[0];
     }
 
 }
