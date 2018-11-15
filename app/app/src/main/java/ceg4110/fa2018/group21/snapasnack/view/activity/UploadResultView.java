32c96520075b429adb8e1c4a90308e95d798236c
Matt Barrett <barrett.77@wright.edu>
2018-11-15 15:12:43 -0500

    Changed empty class out for activity

--- app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/view/activity/UploadResultView.java
+++ app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/view/activity/UploadResultView.java
@@ -1,4 +1,31 @@
 package ceg4110.fa2018.group21.snapasnack.view.activity;
 
-public class UploadResultView {
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import ceg4110.fa2018.group21.snapasnack.R;
+
+public class UploadResultView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_result_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
 }
