3c61daac3c15920ee92c771d150cb4d23b1574cc
Matt Barrett <barrett.77@wright.edu>
2018-11-17 23:25:49 -0500

    Add permissions library. Needs work, but good for now.

--- app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/view/activity/MainActivity.java
+++ app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/view/activity/MainActivity.java
@@ -1,13 +1,20 @@
 package ceg4110.fa2018.group21.snapasnack.view.activity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +25,11 @@ import ceg4110.fa2018.group21.snapasnack.http.callback.PostImagesCallback;
 import ceg4110.fa2018.group21.snapasnack.model.seefood.SeeFoodImage;
 import gun0912.tedbottompicker.TedBottomPicker;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
+
+    private boolean allPermissionsGranted;
+
+    private ArrayList<Button> mainMenuButtons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -29,10 +39,16 @@ public class MainActivity extends AppCompatActivity
 
         // Set up the button listeners programmatically
         configureButtons();
+
+        // Handle permissions through Dexter
+        allPermissionsGranted = false;
+        requestPermissionsFromUser();
     }
 
     public void configureButtons()
     {
+        mainMenuButtons = new ArrayList<>();
+
         Button toGalleryView = (Button) findViewById(R.id.galleryview);
         toGalleryView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +56,7 @@ public class MainActivity extends AppCompatActivity
                 startActivity(new Intent(MainActivity.this, GalleryView.class));
             }
         });
+        mainMenuButtons.add(toGalleryView);
 
         Button uploadImagesButton = (Button) findViewById(R.id.uploadImagesButton);
         uploadImagesButton.setOnClickListener(new View.OnClickListener() {
@@ -48,9 +65,48 @@ public class MainActivity extends AppCompatActivity
                 uploadImages();
             }
         });
+        mainMenuButtons.add(uploadImagesButton);
+    }
+
+    private void requestPermissionsFromUser() {
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA
+                )
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        // Update our global permissions variable
+                        allPermissionsGranted = report.areAllPermissionsGranted();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
+                    }
+                })
+                .check();
     }
 
     public void uploadImages() {
+        // TODO : Clean this up, does not actually check permissions every time (permanently disabled)
+        // Verify permissions and exit if the permissions are not given
+        requestPermissionsFromUser();
+        if(!allPermissionsGranted) {
+            Toast.makeText(this, "Snap a' Snack cannot perform the requested " +
+                    "operation without proper permissions", Toast.LENGTH_LONG);
+            return;
+        }
+
+        // Disable buttons
+        for(Button btn : mainMenuButtons) {
+            btn.setEnabled(false);
+        }
+
+        // If correct permissions are given, continue
         TedBottomPicker bottomSheetDialogFragment = new TedBottomPicker.Builder(MainActivity.this)
                 .setOnMultiImageSelectedListener(new TedBottomPicker.OnMultiImageSelectedListener() {
                     @Override
@@ -67,32 +123,46 @@ public class MainActivity extends AppCompatActivity
                                 @Override
                                 public void onSuccess(@NonNull List<SeeFoodImage> images) {
                                     // TODO : Call Brian's ResultView activity here
-                                    Log.v("POST", "Images posted: " + images.size());
+                                    // Enable the buttons again
+                                    for(Button btn : mainMenuButtons) {
+                                        btn.setEnabled(true);
+                                    }
                                 }
 
                                 @Override
                                 public void onFailure(@NonNull Throwable throwable) {
                                     // TODO : Handle failures and errors
+                                    // Enable the buttons again
+                                    for(Button btn : mainMenuButtons) {
+                                        btn.setEnabled(true);
+                                    }
                                 }
 
                                 @Override
                                 public void onError(@NonNull String errorMessage) {
                                     // TODO : Handle failures and errors
+                                    // Enable the buttons again
+                                    for(Button btn : mainMenuButtons) {
+                                        btn.setEnabled(true);
+                                    }
                                 }
                             });
                         } else {
-                            // TODO: Handle the case of no images being selected
+                            // TODO: This is NOT fired when no images are selected...find another route
+                            // Enable the buttons again
+                            for(Button btn : mainMenuButtons) {
+                                btn.setEnabled(true);
+                            }
                         }
                     }
                 })
                 .setPeekHeight(1600)
                 .showTitle(false)
-                .setCompleteButtonText("Done")
-                .setEmptySelectionText("No Select")
+                .setCompleteButtonText("Submit to AI")
+                .setEmptySelectionText("No Images Selected")
                 .create();
 
         bottomSheetDialogFragment.show(getSupportFragmentManager());
-
     }
 
 }
