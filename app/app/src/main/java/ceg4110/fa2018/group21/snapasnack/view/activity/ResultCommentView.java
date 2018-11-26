ebb622554e3c3da5f7b0c949cd0f95369337a138
Brian <bdinh98@gmail.com>
2018-11-26 11:41:20 -0500

    SeeFoodImage pops up in seperate activity when clicked on in GalleryView

--- app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/view/activity/ResultCommentView.java
+++ app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/view/activity/ResultCommentView.java
@@ -1,9 +1,23 @@
 package ceg4110.fa2018.group21.snapasnack.view.activity;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 import ceg4110.fa2018.group21.snapasnack.R;
+import ceg4110.fa2018.group21.snapasnack.http.SeeFoodAPI;
+import ceg4110.fa2018.group21.snapasnack.http.SeeFoodHTTPHandler;
+import ceg4110.fa2018.group21.snapasnack.http.callback.FetchAllImagesCallback;
+import ceg4110.fa2018.group21.snapasnack.http.callback.FetchSingleImageCallback;
+import ceg4110.fa2018.group21.snapasnack.model.seefood.SeeFoodImage;
+import ceg4110.fa2018.group21.snapasnack.view.adapter.GalleryViewAdapter;
 
 public class ResultCommentView extends AppCompatActivity {
 
@@ -13,5 +27,68 @@ public class ResultCommentView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_comment_view);
+        retrieveAndSetup();
     }
+
+    private void retrieveAndSetup()
+    {
+        if(getIntent().hasExtra("SeeFoodResult"))
+        {
+//            int seeFoodID = getIntent().getIntExtra("SeeFoodResultID", 0);
+
+            SeeFoodImage viewThis = (SeeFoodImage) getIntent().getSerializableExtra("SeeFoodResult");
+
+            setImage(viewThis);
+            setGauge(viewThis);
+            setComments(viewThis);
+
+//            SeeFoodHTTPHandler.getInstance().fetchSingleImage(seeFoodID, new FetchSingleImageCallback()
+//            {
+//                @Override
+//                public void onSuccess(@NonNull SeeFoodImage image)
+//                {
+//                    setImage(image);
+//                    setGauge(image);
+//                    setComments(image);
+//                }
+//
+//                @Override
+//                public void onFailure(@NonNull Throwable throwable)
+//                {
+//
+//                }
+//
+//                @Override
+//                public void onError(@NonNull String errorMessage)
+//                {
+//
+//                }
+//            });
+
+
+        }
+    }
+
+    private void setImage(SeeFoodImage image)
+    {
+        ImageView imageView = findViewById(R.id.result_comment_image);
+        Picasso.get().load(SeeFoodAPI.BASE_URL + image.getImageLocation()).into(imageView);
+    }
+
+    private void setGauge(SeeFoodImage image)
+    {
+
+    }
+    private void setComments(SeeFoodImage image)
+    {
+        TextView textView = findViewById(R.id.result_comments);
+        textView.setText("Testing");
+    }
+
+    // will need to request
+    public void addComment()
+    {
+
+    }
+
 }
--- app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/view/adapter/GalleryViewAdapter.java
+++ app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/view/adapter/GalleryViewAdapter.java
@@ -1,6 +1,7 @@
 package ceg4110.fa2018.group21.snapasnack.view.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -14,11 +15,13 @@ import android.widget.Toast;
 import com.ntt.customgaugeview.library.GaugeView;
 import com.squareup.picasso.Picasso;
 
+import java.io.Serializable;
 import java.util.List;
 
 import ceg4110.fa2018.group21.snapasnack.R;
 import ceg4110.fa2018.group21.snapasnack.http.SeeFoodAPI;
 import ceg4110.fa2018.group21.snapasnack.model.seefood.SeeFoodImage;
+import ceg4110.fa2018.group21.snapasnack.view.activity.ResultCommentView;
 
 public class GalleryViewAdapter extends RecyclerView.Adapter<GalleryViewAdapter.ViewHolder>
 {
@@ -55,11 +58,15 @@ public class GalleryViewAdapter extends RecyclerView.Adapter<GalleryViewAdapter.
             @Override
             public void onClick(View view)
             {
-                //TODO: (???) open a new activity to show full image and related information.
+                //TODO: open a new activity to show full image and related information.
                 Toast.makeText(context, " image", Toast.LENGTH_SHORT).show();
 
                 // TODO: Pass this variable to the new activity and call "fetch single image"
-                int id = galleryList.get(i).getId();
+                SeeFoodImage passThisResult = galleryList.get(i);
+
+                Intent intent = new Intent(context, ResultCommentView.class);
+                intent.putExtra("SeeFoodResult", (Serializable) passThisResult);
+                context.startActivity(intent);
                 
             }
         });
