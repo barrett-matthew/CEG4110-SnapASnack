e7a4be66ed4189b43ddbf7199ca01acd429a73e9
Brian <bdinh98@gmail.com>
2018-12-02 11:53:31 -0500

    ResultView UI cleanup

--- app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/view/activity/ResultView.java
+++ app/app/src/main/java/ceg4110/fa2018/group21/snapasnack/view/activity/ResultView.java
@@ -4,10 +4,11 @@ import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.ntt.customgaugeview.library.GaugeView;
 import com.squareup.picasso.Picasso;
@@ -22,16 +23,34 @@ import ceg4110.fa2018.group21.snapasnack.model.seefood.SeeFoodImage;
 public class ResultView extends AppCompatActivity {
 
     private Button toCommentView;
+    private TextView resultCommentText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result_view);
-        retrieveAndSetup();
+
+        setupActivity();
+
     }
 
-    private void retrieveAndSetup() {
-        if (getIntent().hasExtra("SeeFoodResult")) {
+    private void setupActivity()
+    {
+        Toolbar toolbar = findViewById(R.id.resultToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Results");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+                                                 @Override
+                                                 public void onClick(View view)
+                                                 { finish();
+                                                 }
+                                             }
+        );
+
+        if (getIntent().hasExtra("SeeFoodResult"))
+        {
             SeeFoodImage viewThis = (SeeFoodImage) getIntent().getSerializableExtra("SeeFoodResult");
             setImage(viewThis);
             setGauge(viewThis);
@@ -39,13 +58,19 @@ public class ResultView extends AppCompatActivity {
         }
     }
 
-    private void setImage(final SeeFoodImage image) {
-        ImageView imageView = findViewById(R.id.result_comment_image);
-        Picasso.get().load(SeeFoodAPI.BASE_URL + image.getImageLocation()).into(imageView);
+    private void setImage(final SeeFoodImage image)
+    {
+        ImageView seeFoodImage = findViewById(R.id.result_comment_image);
+        Picasso.get().load(SeeFoodAPI.BASE_URL + image.getImageLocation()).into(seeFoodImage);
+
+        ImageView aiImage = findViewById(R.id.seeFoodAIPicture);
+        Picasso.get().load("https://cdn1.iconfinder.com/data/icons/artificial-intelligence-1-2/128/Brain-Technology-Intelligence-Engineering-Scientific-Neuroscience-512.png").into(aiImage);
     }
 
-    private void setGauge(final SeeFoodImage image) {
+    private void setGauge(final SeeFoodImage image)
+    {
         final GaugeView gaugeView = findViewById(R.id.result_comment_gauge);
+        resultCommentText = findViewById(R.id.resultCommentText);
 
         gaugeView.setShowRangeValues(false);
 
@@ -54,30 +79,54 @@ public class ResultView extends AppCompatActivity {
         // The timer will allow the gauge to fluctuate between values before setting on a final target value
         final CountDownTimer timer = new CountDownTimer(4000, 2) {
             @Override
-            public void onTick(long millisUntilFinished) {
+            public void onTick(long millisUntilFinished)
+            {
                 gaugeView.setTargetValue(random.nextInt(100));
             }
 
             @Override
-            public void onFinish() {
+            public void onFinish()
+            {
                 float result = image.getHasFood() - image.getNotFood();
 
                 // calculation to set gauge based on SeeFood AI results (calculating percentage using hasFood and notFood)
                 if (result > 5) {
                     gaugeView.setTargetValue(99);
+                    resultCommentText.setText("AI says: This image definitely contains food! :-D");
                 } else if (result < -3) {
                     gaugeView.setTargetValue(0);
+                    resultCommentText.setText("AI says: This image is definitely not food! D-:");
                 } else {
                     gaugeView.setTargetValue(((result + 3) / 8) * 100);
+                    setIntelligenceDialog(((result + 3) / 8) * 100);
                 }
-
             }
         };
-
         timer.start();
     }
 
-    private void setButton(final SeeFoodImage image) {
+    private void setIntelligenceDialog(float confidenceRating)
+    {
+        if(confidenceRating >= 90)
+        {
+            resultCommentText.setText("AI says: This image has a " + confidenceRating + "% chance of being food! :-D");
+        }
+        else if (confidenceRating >= 60)
+        {
+            resultCommentText.setText("AI says: This image has a " + confidenceRating + "% chance of being food! :-)");
+        }
+        else if (confidenceRating >= 30)
+        {
+            resultCommentText.setText("AI says: This image has a " + confidenceRating + "% chance of being food! :-(");
+        }
+        else
+        {
+            resultCommentText.setText("AI says: This image has a " + confidenceRating + "% chance of being food! :,-(");
+        }
+    }
+
+    private void setButton(final SeeFoodImage image)
+    {
         toCommentView = findViewById(R.id.toCommentView);
         toCommentView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,11 +141,12 @@ public class ResultView extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
 
-        //TODO: maybe add swiping motions to change gallery images? (use fetchSingleImage)
+    //TODO: maybe add swiping motions to change gallery images? (use fetchSingleImage)
 
-        //            SeeFoodHTTPHandler.getInstance().fetchSingleImage(seeFoodID, new FetchSingleImageCallback()
+    //            SeeFoodHTTPHandler.getInstance().fetchSingleImage(seeFoodID, new FetchSingleImageCallback()
 //            {
 //                @Override
 //                public void onSuccess(@NonNull SeeFoodImage image)
@@ -119,5 +169,5 @@ public class ResultView extends AppCompatActivity {
 //                }
 //            });
 
-    }
+
 }
